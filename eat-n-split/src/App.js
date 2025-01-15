204eb62936ca1bdd96652b83e06ae8e6f087da03
Gaurav Raj <gauravraj0811@gmail.com>
2025-01-16 00:12:43 +0530

    update

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -85,13 +85,39 @@ function Button({ children, onClick }) {
 }
 
 function FormAddFriend({ addFriendButton }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?={id}`,
+      balance: 0,
+    };
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🧑‍🤝‍🧑Friend Name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>ℹ️Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button onClick={addFriendButton}>Add</Button>
     </form>
